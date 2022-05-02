97bfa782f99a0712c89e15b27ea9d25b6308bf32
ohju96 <ohju96@naver.com>
2022-05-02 12:09:06 +0900

    blog, single_pages 셋팅

--- djangoPrj/settings.py
+++ djangoPrj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l+f%4c8&e$k85)8vl(i4r9$^0k)=ubcir+c2+uvm*o&u%-&q8%'
+SECRET_KEY = 'django-insecure-)wv3di6rwr7ppku_v1qze_wuc*3j9l)r1+&6e%yt(#31*938)-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

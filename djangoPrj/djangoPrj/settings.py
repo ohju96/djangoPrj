1467bd2ae8ab37242dea4fb72300322718cb1c7d
ohju96 <ohju96@naver.com>
2022-05-02 10:43:13 +0900

    CLI로 생성

--- djangoPrj/djangoPrj/settings.py
+++ djangoPrj/djangoPrj/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l+f%4c8&e$k85)8vl(i4r9$^0k)=ubcir+c2+uvm*o&u%-&q8%'
+SECRET_KEY = 'django-insecure-ik^(5#60%xy^20o#8h8p85mg1&xwnw4exfu3!am+5&29f6wram'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
